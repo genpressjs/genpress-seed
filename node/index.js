2dc6927b849b218e24b797f48c67aed4ba142495
Michael F <tehfreak@awesome39.com>
2014-03-20 16:49:39 +0300

    added auth example

--- node/index.js
+++ node/index.js
@@ -2,11 +2,19 @@ var App= require('general-express')
 var app= module.exports= App()
 
 
+// middlewares
+
+app.use('/api', require('Example/handlers/Cookie').init)
+app.use('/api', require('Example/handlers/Session').init)
+
+app.use('/api', require('Example/handlers/Auth').init)
+app.use('/api', require('Example/handlers/Auth').initSession)
+
+// subapps
 
 app.use('/api/v1/users'
 ,   require('Example/handlers/Api/Users')
 )
-
 
 
 app.listen(7001)
